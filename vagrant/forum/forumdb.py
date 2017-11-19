e4cba29ea89e8cdf0e49c1e64534f8c2dfd4469c
Thaissa Falbo <thaissa@Thaissas-MacBook-Pro.local>
2017-11-19 19:22:46 -0200

    connects to db

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,21 @@
 # "Database code" for the DB Forum.
 
-import datetime
+import psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("select content,time from posts order by time desc")
+  return c.fetchall()
+  db.close()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("insert into posts values('%s')" % content)
+  db.commit()
+  db.close()
