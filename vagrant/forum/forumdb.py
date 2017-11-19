1a5d6ccf02bec303d454f87a6bb39baed30c205f
Thaissa Falbo <thaissa@thaissa.eng.br>
2017-11-19 19:24:03 -0200

    prevents sql injections

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -16,6 +16,6 @@ def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute("insert into posts values('%s')" % content)
+  c.execute("insert into posts values(%s)",(content,))
   db.commit()
   db.close()
