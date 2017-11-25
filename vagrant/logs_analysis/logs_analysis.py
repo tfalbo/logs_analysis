0fe14b18b464003e020fd9b10825f9825b6c98da
Thaissa Falbo <thaissa@thaissa.eng.br>
2017-11-25 01:03:38 -0200

    PEP8 corrections

--- vagrant/logs_analysis/logs_analysis.py
+++ vagrant/logs_analysis/logs_analysis.py
@@ -5,7 +5,7 @@ import psycopg2
 DBNAME = "news"
 
 # What are the most popular three articles of all time?
-most_popular_articles =  """select articles.title, count(*) as num
+most_popular_articles = """select articles.title, count(*) as num
     from articles, log
     where log.status like '%200%'
     and log.path like concat('/article/', articles.slug)
@@ -30,14 +30,10 @@ errors_with_correct_slug = """
 
 
 queries = {
-     'What are the most popular three articles of all time?'
-        : most_popular_articles,
-     'Who are the most popular article authors of all time?'
-        : most_popular_authors,
-     'Are there any errors when the correct slug is requested?'
-        : errors_with_correct_slug
-     }
-
+    'What are the most popular three articles of all time?': most_popular_articles,
+    'Who are the most popular article authors of all time?': most_popular_authors,
+    'Are there any errors when the correct slug is requested?': errors_with_correct_slug
+}
 
 
 def connect():
@@ -45,24 +41,25 @@ def connect():
     try:
         db = psycopg2.connect(database=DBNAME)
         c = db.cursor()
-        return db,c
+        return db, c
     except:
-       "Unable to connect to database"
+        "Unable to connect to database"
+
 
 def make_query(query):
     """Makes query and return results"""
-    db,c = connect()
+    db, c = connect()
     c.execute(query)
     return c.fetchall()
     db.close()
 
 
 if __name__ == "__main__":
-    for k,v in queries.items():
+    for k, v in queries.items():
         print k
         res = make_query(v)
         if not res:
             print 'No results'
         else:
-            for i,j in make_query(v):
+            for i, j in make_query(v):
                 print i + ' - ' + str(j)
